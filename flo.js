4db6a968ad89eceba302a6899a3b12906177d5b9
HuttonR <hutton.rounds@thebronson.com>
2016-02-01 11:57:45 -0800

    Up and running.

--- flo.js
+++ flo.js
@@ -1,42 +1,52 @@
-var _registered = []
+var _events = []
 
 Flo = {
-  register(context, type, callback) {
+  register(type, callback, context) {
     if (arguments.length === 1 && typeof arguments[0] === 'object') {
-      context  = arguments[0].context
-      type     = arguments[0].type
-      callback = arguments[0].callback
+      let arg = arguments[0]
+
+      type     = arg.type
+      callback = arg.callback
+      context  = arg.context
     }
 
-    if (!context)  console.warn('Using Flo.register without a context is not advised as it cannot be unregistered.')
-    if (!type)     throw new Error('You must supply Flo.register with a specific event type (string) to register.')
-    if (!callback) throw new Error('You must supply Flo.register with a data callback to receive the event data.')
+    if (!type)     throw new Error('You must supply Flo.register with a specific event type (string) to register')
+    if (!callback) throw new Error('You must supply Flo.register with a callback to receive the event')
 
-    if (typeof type !== 'string')       throw new Error('Event type supplied to Flo.register must be a string.')
-    if (typeof callback !== 'function') throw new Error('Data callback supplied to Flo.register must be a function')
+    if (context && typeof context !== 'object') throw new Error('Context supplied to Flo.register must be an object')
 
-    if (context && _registered.find(e => (
-      e.context === context &&
-      e.type === type
-    ))) {
-      throw new Error(`Already registered event of type: '${type}' for this context.`)
-    }
+    if (typeof type !== 'string')       throw new Error('Event type supplied to Flo.register must be a string')
+    if (typeof callback !== 'function') throw new Error('Callback supplied to Flo.register must be a function')
 
-    _registered.push({context, type, callback})
+    let id = Random.id()
+
+    _events.push({id, type, callback, context})
+    if (Flo._expose) Flo._events = _events
+
+    return id
   },
 
-  unregister(context, type) {
-    if (type) {
-      //
+  unregister(idOrContext) {
+    if (typeof idOrContext === 'string') {
+      let index = _events.findIndex(e => (e.id === idOrContext))
+      if (index >= 0) _events.splice(index, 1)
+    } else if (idOrContext instanceof Array) {
+      _events = _events.filter(e => (idOrContext.indexOf(e.id) === -1))
+    } else if (typeof idOrContext === 'object') {
+      _events = _events.filter(e => (e.context !== idOrContext))
+    } else {
+      throw new Error('You must supply an id (or an array of ids) or a context to Flo.unregister')
     }
+
+    if (Flo._expose) Flo._events = _events
   },
 
-  trigger(type, data) {
-    //
+  trigger(type, ...args) {
+    if (!type)                    throw new Error('You must supply Flo.trigger with a specific event to trigger')
+    if (typeof type !== 'string') throw new Error('Event supplied to Flo.trigger must be a string')
+
+    for (let e of _events) {
+      if (e.type === type) e.callback.apply(e.context || this, args)
+    }
   },
 }
-
-
-// Expose these temporarily
-Flo._ = {}
-Flo._._registered = _registered

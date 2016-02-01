192b229d45c8704d8bb1423ec9ca6c64c8b2f07c
HuttonR <hutton.rounds@thebronson.com>
2016-01-31 21:18:41 -0800

    Empty but ready for code.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'huttonr:flo',
   version: '0.0.1',
   summary: 'An intuitive event handler.',
-  git: '',
+  git: 'https://github.com/huttonr/flo',
   documentation: 'README.md'
 });
 
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
 
-  api.export(['ReactMeteorComputations'])
+  api.export(['Flo'])
 
   api.addFiles('flo.js');
 });
